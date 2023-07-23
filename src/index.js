eb18b5f25b9278cdcc54bb1791ba202905c38374
Lilly <lillianaragundi@gmail.com>
2023-07-23 17:45:39 -0500

    added js and moment for main dates and time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
+function updateTime() {
+  //New York
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Buenos Aires
+  let buenosAiresElement = document.querySelector("#buenos-aires");
+  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+  let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
+
+  buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
+  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Copenhagen
+  let copenhagenElement = document.querySelector("#copenhagen");
+  let copenhagenDateElement = copenhagenElement.querySelector(".date");
+  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  let copenhagenTime = moment().tz("Europe/Copenhagen");
+
+  copenhagenDateElement.innerHTML = copenhagenTime.format("MMMM Do YYYY");
+  copenhagenTimeElement.innerHTML = copenhagenTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
